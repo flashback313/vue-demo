c825fb80a4291c359ee7b4b5d6a351572e5ae0a3
alex <alex@alexdeMacBook-Pro.local>
2016-07-14 16:07:42 +0800

    init vue loader

---
+++ src/component/HeadComment.vue
@@ -0,0 +1,19 @@
+<template>
+	<h1>{{title}}</h1>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			title: 'Hello from vue-loader!'
+		}
+	}
+}
+</script>
+
+<style type="text/css">
+	h1 {
+		font-size: 18px;
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import HeadComment from './component/HeadComment.vue'
+
+/* eslint-disable no-new */
+new Vue({
+	el: 'body',
+	components: { HeadComment }
+})
